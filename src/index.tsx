3acc5e6c29a63109dd04175e9f73fd09c6ae1ab0
iker ocio <iker@ikerocio.com>
2023-09-05 22:53:51 +0200

    Google fonts added by npm package

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+  
+  body {
+    font-family: 'Lato', sans-serif; /* Utiliza la fuente cargada */
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const root = document.getElementById("root");
 const appRoot = ReactDOM.createRoot(root as HTMLElement);
 
 appRoot.render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
