20d9fc991f1869d6e99297842428209c5dace900
iker ocio <iker@ikerocio.com>
2023-09-05 21:20:37 +0200

    About updated

--- src/components/About.js
+++ src/components/About.js
@@ -8,16 +8,26 @@ const About = forwardRef(({ aboutStatus, setAboutStatus }, ref) => {
         <H1>Sobre este proyecto</H1>
         <P>
           Esta plataforma nace de un profundo respeto y admiración por la música
-          rap que resonó en Vitoria y sus alrededores durante los años 2000.
-          Aquellos tiempos, marcados por ritmos y letras inolvidables, formaron
-          una parte esencial de nuestra infancia y juventud. Con esta web, no
-          solo buscamos rendir homenaje a esos artistas y sus creaciones, sino
-          también ofrecer un espacio de reencuentro con esos momentos. Es
-          importante mencionar que no buscamos infringir derechos ni molestar a
-          los artistas involucrados. Si algún artista prefiere no estar
-          representado aquí, solo necesita ponerse en contacto con uno de los
-          autores, ya sea Iker Ocio o Jonzo, y atenderemos su petición de
-          inmediato.
+          rap que resonó en Vitoria y sus alrededores durante los años
+          <Remark href="#">2000-2010</Remark>. Aquellos tiempos, marcados por
+          ritmos y letras inolvidables, formaron una parte esencial de nuestra
+          infancia y juventud. Con esta web, no solo buscamos rendir homenaje a
+          esos artistas y sus creaciones, sino también ofrecer un espacio de
+          reencuentro con esos momentos. Es importante mencionar que no buscamos
+          infringir derechos ni molestar a los artistas involucrados. Si algún
+          artista prefiere no estar representado aquí, solo necesita ponerse en
+          contacto con uno de los autores, ya sea{" "}
+          <Remark href="https://ikerocio.com" target="_blank">
+            Iker Ocio Zuazo
+          </Remark>{" "}
+          o
+          <Remark
+            href="https://www.instagram.com/jonzoonegraphics/"
+            target="_blank"
+          >
+            JonzoOne
+          </Remark>
+          , y atenderemos su petición de inmediato.
         </P>
         <H2>Motivación detrás del proyecto</H2>
         <P>
@@ -32,27 +42,46 @@ const About = forwardRef(({ aboutStatus, setAboutStatus }, ref) => {
         <P>
           Conscientes de la importancia de la eficiencia y el respeto por el
           consumo de datos en la era digital, hemos optado por exportar todos
-          los archivos musicales al formato m4a. Esta decisión permite una
-          reproducción de alta calidad mientras se mantiene un tamaño de archivo
-          reducido, garantizando así una experiencia de usuario óptima.
+          los archivos musicales al formato <Remark href="#">m4a</Remark>. Esta
+          decisión permite una reproducción de alta calidad mientras se mantiene
+          un tamaño de archivo reducido, garantizando así una experiencia de
+          usuario óptima.
         </P>
         <H2>Tecnología y diseño</H2>
         <P>
-          Esta web ha sido desarrollada utilizando React, una de las bibliotecas
-          más populares y potentes para la creación de interfaces de usuario. Al
-          no contar con un backend, toda la lógica y el contenido se ejecutan
-          directamente en el navegador del usuario, lo que garantiza una
-          experiencia fluida y rápida.
+          Esta web ha sido desarrollada utilizando{" "}
+          <Remark href="#">React</Remark>, una de las bibliotecas más populares
+          y potentes para la creación de interfaces de usuario. Al no contar con
+          un backend, toda la lógica y el contenido se ejecutan directamente en
+          el navegador del usuario, lo que garantiza una experiencia fluida y
+          rápida.
         </P>
       </AboutContainer>
       <CloseButton
         $aboutStatus={aboutStatus}
-        onClick={() => setAboutStatus(false)}>
+        onClick={() => setAboutStatus(false)}
+      >
         X
       </CloseButton>
     </>
   );
 });
+
+const Remark = styled.a`
+  font-style: normal;
+  background: #414141;
+  color: #fff;
+  padding: 0 0.25rem;
+  text-decoration:none;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  margin:.25rem;
+
+  &:hover {
+    background: #d9d9d9;
+    color: #414141;
+  }
+}
+`;
 
 const AboutContainer = styled.div`
   position: fixed;
